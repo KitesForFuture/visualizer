faedb79835ea53c408926ce7f9b5aa0b64a3799d
LeonardKoll <leonard.koll@netlight.com>
2021-06-29 13:08:11 +0200

    height_derivative

--- flydata.py
+++ flydata.py
@@ -4,13 +4,15 @@ class Flydata():
 
     def __init__(self, bytes):
         # Cycle-Time (1), Height (1), Gyro-Vector (3), Accel-Vector (3), Rotation-Matrix (9), G-Correction-Axis (3), G-Correction-Angle (1), Position-Matrix (9)
-        data = struct.unpack('ffffffffffffffffffffffffffffff', bytes)
+        data = struct.unpack('fffffffffffffffffffffffffffffff', bytes)
         self.cycle_seconds = data[0]
         self.height = data[1]
-        self.x_rotation = (data[21], data[24], data[27])
-        self.y_rotation = (data[22], data[25], data[28])
-        self.z_rotation = (data[23], data[26], data[29])
-
+        self.height_derivative = data[2]
+        self.x_rotation = (data[22], data[25], data[28])
+        self.y_rotation = (data[23], data[26], data[29])
+        self.z_rotation = (data[24], data[27], data[30])
+        print("Accel: " + str(data[6:9]))
+        #print("Axis / Angle:" + str(data[17:21]))
 
     @property
     def cycle_seconds(self):
